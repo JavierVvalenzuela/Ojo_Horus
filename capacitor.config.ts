3d53e2b8d05764da213512f043aab3c6ba746ab4
diego <di.mellado@duocuc.cl>
2025-02-02 01:21:39 -0300

    icon?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Ojo_Horus',
+  appName: 'Horus',
   webDir: 'www'
 };
 
